b89dbe509381f83d5dfa28c9f9389e5658ad833a
Pablo Parejo <parejo@Pablos-MacBook-Pro.local>
2015-03-28 11:06:47 +0100

    Initial Commit

---
+++ chineseStore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chineseStore
+//
+//  Created by Pablo Parejo Camacho on 28/3/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ chineseStore/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  chineseStore
+//
+//  Created by Pablo Parejo Camacho on 28/3/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
