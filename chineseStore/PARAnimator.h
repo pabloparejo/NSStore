cd944aaf5df13b6d0d054e1f91d42e6137ba0ce1
Pablo Parejo Camacho <parejo.p@gmail.com>
2015-04-11 10:08:06 +0200

    add animator

---
+++ chineseStore/PARAnimator.h
@@ -0,0 +1,13 @@
+//
+//  PARAnimator.h
+//  chineseStore
+//
+//  Created by Pablo Parejo Camacho on 10/4/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PARAnimator : NSObject<UIViewControllerAnimatedTransitioning>
+
+@end
---
+++ chineseStore/PARAnimator.m
@@ -0,0 +1,13 @@
+//
+//  PARAnimator.m
+//  chineseStore
+//
+//  Created by Pablo Parejo Camacho on 10/4/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import "PARAnimator.h"
+
+@implementation PARAnimator
+
+@end
